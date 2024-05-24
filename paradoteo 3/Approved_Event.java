f8aa496b74990d29af51e5c983f2b983ff737e27
IOANNIS KOLLIAS <85553551+KolliasIoannisK@users.noreply.github.com>
2024-05-24 17:35:46 +0300

    Add files via upload

---
+++ paradoteo 3/Approved_Event.java
@@ -0,0 +1,155 @@
+import java.sql.* ;
+import javax.swing.*;
+
+
+/**
+ * Γράψτε μια περιγραφή της κλάσης Approved_Event εδώ.
+ * 
+ * @author (Το όνομά σας) 
+ * @version (Αριθμός έκδοσης ή ημερομηνία εδώ)
+ */
+public class Approved_Event extends Event
+{
+    // μεταβλητές στιγμιοτύπου - αντικαταστήστε το ακόλουθο παράδειγμα
+    // με τις δικές σας μεταβλητές
+    int expected_people;
+
+    /**
+     * Κατασευαστής αντικειμένων της κλάσης Approved_Event
+     */
+    public Approved_Event()
+    {
+        // αρχικοποίηση μεταβλητών στιγμιοτύπου
+        
+    }
+
+    /**
+     * Παράδειγμα μεθόδου - αντικαταστήστε το παρόν σχόλιο με το δικό σας
+     * 
+     * @param  y    παράδειγμα παραμέτρου για την μέθοδο
+     * @return        το άθροισμα του x με το y 
+     */
+    public void conclude()
+    {
+        try
+    {
+       // Load the database driver
+       Class.forName( "com.mysql.cj.jdbc.Driver" ) ;
+
+       // Get a connection to the database
+       Connection conn = DriverManager.getConnection( "jdbc:mysql://localhost/demo?user=root&password=terrorism" ) ;
+
+       // Print all warnings
+       for( SQLWarning warn = conn.getWarnings(); warn != null; warn = warn.getNextWarning() )
+       {
+          System.out.println( "SQL Warning:" ) ;
+          System.out.println( "State  : " + warn.getSQLState()  ) ;
+          System.out.println( "Message: " + warn.getMessage()   ) ;
+          System.out.println( "Error  : " + warn.getErrorCode() ) ;
+       }
+        
+       Finished_Event  event = new Finished_Event();
+       event.title=this.title;
+       event.description=this.description;
+       event.time=this.time;
+       event.duration=this.duration;
+       event.organizer=this.organizer;
+       event.participant_number=this.expected_people;
+       
+       event.save();
+       PreparedStatement stmt = conn.prepareStatement("delete from events where title=? ");
+       stmt.setString(1, this.title);
+       stmt.executeUpdate();
+
+       // Loop through the result set
+       
+       conn.close() ;
+   }
+   catch( SQLException se )
+   {
+       System.out.println( "SQL Exception:" ) ;
+
+       // Loop through the SQL Exceptions
+       while( se != null )
+       {
+          System.out.println( "State  : " + se.getSQLState()  ) ;
+          System.out.println( "Message: " + se.getMessage()   ) ;
+          System.out.println( "Error  : " + se.getErrorCode() ) ;
+
+          se = se.getNextException() ;
+       }
+   }
+   catch( Exception e )
+   {
+      System.out.println( e ) ;
+   }
+        
+    }
+    
+    public void save(){
+        try
+    {
+       // Load the database driver
+       Class.forName( "com.mysql.cj.jdbc.Driver" ) ;
+
+       // Get a connection to the database
+       Connection conn = DriverManager.getConnection( "jdbc:mysql://localhost/demo?user=root&password=terrorism" ) ;
+
+       // Print all warnings
+       for( SQLWarning warn = conn.getWarnings(); warn != null; warn = warn.getNextWarning() )
+       {
+          System.out.println( "SQL Warning:" ) ;
+          System.out.println( "State  : " + warn.getSQLState()  ) ;
+          System.out.println( "Message: " + warn.getMessage()   ) ;
+          System.out.println( "Error  : " + warn.getErrorCode() ) ;
+       }
+
+       // Get a statement from the connection
+       ;
+
+       // Execute the query
+           
+       PreparedStatement stmt = conn.prepareStatement("INSERT INTO events(title, description,time, duration,organizer,people) VALUES ( ?, ?,?,?,?,?)");
+        
+       
+       
+        stmt.setString(1, this.title);
+        stmt.setString(2, this.description);
+        stmt.setString(3, this.time);
+        stmt.setInt(4, this.duration);
+        stmt.setString(5, this.organizer.profile.name);
+        stmt.setInt(6, this.expected_people);
+        
+
+        stmt.executeUpdate();
+
+       // Loop through the result set
+       
+
+       // Close the result set, statement and the connection
+       
+       conn.close() ;
+   }
+   catch( SQLException se )
+   {
+       System.out.println( "SQL Exception:" ) ;
+
+       // Loop through the SQL Exceptions
+       while( se != null )
+       {
+          System.out.println( "State  : " + se.getSQLState()  ) ;
+          System.out.println( "Message: " + se.getMessage()   ) ;
+          System.out.println( "Error  : " + se.getErrorCode() ) ;
+
+          se = se.getNextException() ;
+       }
+   }
+   catch( Exception e )
+   {
+      System.out.println( e ) ;
+   }
+        
+    }
+        
+    }
+
---
+++ paradoteo 3/Event_Suggestion.java
@@ -0,0 +1,160 @@
+import java.sql.* ;
+import javax.swing.*;
+/**
+ * Γράψτε μια περιγραφή της κλάσης Event_Suggestion εδώ.
+ * 
+ * @author (Το όνομά σας) 
+ * @version (Αριθμός έκδοσης ή ημερομηνία εδώ)
+ */
+public class Event_Suggestion extends Event
+{
+    // μεταβλητές στιγμιοτύπου - αντικαταστήστε το ακόλουθο παράδειγμα
+    // με τις δικές σας μεταβλητές
+    int expected_people;
+    int likes;
+    int request;
+
+    /**
+     * Κατασευαστής αντικειμένων της κλάσης Event_Suggestion
+     */
+    public Event_Suggestion()
+    {
+        // αρχικοποίηση μεταβλητών στιγμιοτύπου
+        
+    }
+
+    /**
+     * Παράδειγμα μεθόδου - αντικαταστήστε το παρόν σχόλιο με το δικό σας
+     * 
+     * @param  y    παράδειγμα παραμέτρου για την μέθοδο
+     * @return        το άθροισμα του x με το y 
+     */
+    public int getLikes()
+    {
+        return this.likes;
+        
+    }
+    
+    public void approve(){
+        try
+    {
+       // Load the database driver
+       Class.forName( "com.mysql.cj.jdbc.Driver" ) ;
+
+       // Get a connection to the database
+       Connection conn = DriverManager.getConnection( "jdbc:mysql://localhost/demo?user=root&password=terrorism" ) ;
+
+       // Print all warnings
+       for( SQLWarning warn = conn.getWarnings(); warn != null; warn = warn.getNextWarning() )
+       {
+          System.out.println( "SQL Warning:" ) ;
+          System.out.println( "State  : " + warn.getSQLState()  ) ;
+          System.out.println( "Message: " + warn.getMessage()   ) ;
+          System.out.println( "Error  : " + warn.getErrorCode() ) ;
+       }
+        
+       Approved_Event  event = new Approved_Event();
+       event.title=this.title;
+       event.description=this.description;
+       event.time=this.time;
+       event.duration=this.duration;
+       event.organizer=this.organizer;
+       event.expected_people=this.expected_people;
+       
+       event.save();
+       PreparedStatement stmt = conn.prepareStatement("delete from suggestions where title=? ");
+       stmt.setString(1, this.title);
+       stmt.executeUpdate();
+
+       // Loop through the result set
+       
+       conn.close() ;
+   }
+   catch( SQLException se )
+   {
+       System.out.println( "SQL Exception:" ) ;
+
+       // Loop through the SQL Exceptions
+       while( se != null )
+       {
+          System.out.println( "State  : " + se.getSQLState()  ) ;
+          System.out.println( "Message: " + se.getMessage()   ) ;
+          System.out.println( "Error  : " + se.getErrorCode() ) ;
+
+          se = se.getNextException() ;
+       }
+   }
+   catch( Exception e )
+   {
+      System.out.println( e ) ;
+   }
+        
+        
+    }
+    
+    public void saveEvent(){
+        try
+    {
+       // Load the database driver
+       Class.forName( "com.mysql.cj.jdbc.Driver" ) ;
+
+       // Get a connection to the database
+       Connection conn = DriverManager.getConnection( "jdbc:mysql://localhost/demo?user=root&password=terrorism" ) ;
+
+       // Print all warnings
+       for( SQLWarning warn = conn.getWarnings(); warn != null; warn = warn.getNextWarning() )
+       {
+          System.out.println( "SQL Warning:" ) ;
+          System.out.println( "State  : " + warn.getSQLState()  ) ;
+          System.out.println( "Message: " + warn.getMessage()   ) ;
+          System.out.println( "Error  : " + warn.getErrorCode() ) ;
+       }
+
+       // Get a statement from the connection
+       ;
+
+       // Execute the query
+           
+       PreparedStatement stmt = conn.prepareStatement("INSERT INTO suggestions(title, description,time, duration,organizer,people,likes,request) VALUES (?,?, ?, ?,?,?,?,?)");
+        
+       
+       
+        stmt.setString(1, this.title);
+        stmt.setString(2, this.description);
+        stmt.setString(3, this.time);
+        stmt.setInt(4, this.duration);
+        stmt.setString(5, this.organizer.profile.name);
+        stmt.setInt(6, this.expected_people);
+        stmt.setInt(7, this.likes);
+        stmt.setInt(8, this.request);
+
+        stmt.executeUpdate();
+
+       // Loop through the result set
+       
+
+       // Close the result set, statement and the connection
+       
+       conn.close() ;
+   }
+   catch( SQLException se )
+   {
+       System.out.println( "SQL Exception:" ) ;
+
+       // Loop through the SQL Exceptions
+       while( se != null )
+       {
+          System.out.println( "State  : " + se.getSQLState()  ) ;
+          System.out.println( "Message: " + se.getMessage()   ) ;
+          System.out.println( "Error  : " + se.getErrorCode() ) ;
+
+          se = se.getNextException() ;
+       }
+   }
+   catch( Exception e )
+   {
+      System.out.println( e ) ;
+   }
+        
+    }
+}
