4f7f1f894c8d0b01d6f65b929d7eb00145785d39
LeBronSunshine136 <168530233+LeBronSunshine136@users.noreply.github.com>
2024-05-24 18:24:45 +0300

    Add files via upload
    Tag implemented

---
+++ paradoteo 3/Tag.java
@@ -0,0 +1,15 @@
+package models;
+
+public class Tag {
+    private final String name;
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    public String getName() { return name; }
+
+    public String getTagName() {
+        return name;
+    }
+}
