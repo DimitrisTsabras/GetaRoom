1acba889d38cbabeaf3cb1b5129635cdd0f06357
elenich24 <166219237+elenich24@users.noreply.github.com>
2024-05-26 23:46:57 +0300

    Add files via upload

---
+++ paradoteo 3/Administrator1.java
@@ -0,0 +1,88 @@
+import java.sql.* ;
+import javax.swing.*;
+/**
+ * Γράψτε μια περιγραφή της κλάσης Administrator εδώ.
+ * 
+ * @author (Το όνομά σας) 
+ * @version (Αριθμός έκδοσης ή ημερομηνία εδώ)
+ */
+public class Administrator extends User
+{
+    // μεταβλητές στιγμιοτύπου - αντικαταστήστε το ακόλουθο παράδειγμα
+    // με τις δικές σας μεταβλητές
+    
+
+    /**
+     * Κατασευαστής αντικειμένων της κλάσης Administrator
+     */
+    public Administrator()
+    {
+        // αρχικοποίηση μεταβλητών στιγμιοτύπου
+        
+    }
+
+    /**
+     * Παράδειγμα μεθόδου - αντικαταστήστε το παρόν σχόλιο με το δικό σας
+     * 
+     * @param  y    παράδειγμα παραμέτρου για την μέθοδο
+     * @return        το άθροισμα του x με το y 
+     */
+    public void solve(String request)
+    {
+        try
+    {
+       // Load the database driver
+       Class.forName( "com.mysql.cj.jdbc.Driver" ) ;
+
+       // Get a connection to the database
+       Connection conn = DriverManager.getConnection( "jdbc:mysql://localhost/demo?user=root&password=terrorism" ) ;
+
+       // Print all warnings
+       for( SQLWarning warn = conn.getWarnings(); warn != null; warn = warn.getNextWarning() )
+       {
+          System.out.println( "SQL Warning:" ) ;
+          System.out.println( "State  : " + warn.getSQLState()  ) ;
+          System.out.println( "Message: " + warn.getMessage()   ) ;
+          System.out.println( "Error  : " + warn.getErrorCode() ) ;
+       }
+
+       // Get a statement from the connection
+       ;
+
+       // Execute the query
+        
+        PreparedStatement stmt = conn.prepareStatement("delete from  support where theme=?");
+        stmt.setString(1, request);
+        
+        stmt.executeUpdate();
+        
+        
+
+       // Loop through the result set
+       
+
+       // Close the result set, statement and the connection
+       
+       conn.close() ;
+   }
+   catch( SQLException se )
+   {
+       System.out.println( "SQL Exception:" ) ;
+
+       // Loop through the SQL Exceptions
+       while( se != null )
+       {
+          System.out.println( "State  : " + se.getSQLState()  ) ;
+          System.out.println( "Message: " + se.getMessage()   ) ;
+          System.out.println( "Error  : " + se.getErrorCode() ) ;
+
+          se = se.getNextException() ;
+       }
+   }
+   catch( Exception e )
+   {
+      System.out.println( e ) ;
+   }
+        
+    }
+}
